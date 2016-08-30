0223eb4eb3b8aff4f64886615cb1989650086ad1
guoxj1228 <guoxj1228@163.com>
2016-08-30 18:09:21 +0800

    update test

--- src/com/atguigu/testgit/Hello.java
+++ src/com/atguigu/testgit/Hello.java
@@ -1,5 +1,11 @@
 package com.atguigu.testgit;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello");
@@ -14,3 +20,102 @@ public class Hello {
 		System.out.println("w测试冲突");
 	}
 }
+
+class TestList{
+	public static void main(String[] args)
+	{
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(1);
+		list.add(2);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		System.out.println("before:  "+list.size());
+		System.out.println("after:  "+DuplicateListDemo.dupDeal(list).size());
+		list = DuplicateListDemo.dupDeal(list);
+		for (Integer element : list){
+			System.out.print(element+" ");
+		}
+		System.out.println();
+		
+	}
+	
+}
+
+class DuplicateListDemo{
+	public static List<Integer> dupDeal(List<Integer> list) {
+		
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < list.size(); i++) {
+			set.add(list.get(i));
+		}
+		list.clear();
+		for (Iterator iterator = set.iterator(); iterator.hasNext();) {
+			Integer integer = (Integer) iterator.next();
+			list.add(integer);
+		}
+		return list;
+	}
+}
+
+
+
+
+
+class  smallT
+{
+	public static void  main(String args[])
+	{
+		smallT t  = new  smallT();
+		int  b  =  t.get();
+		System.out.println(b);
+	}
+	public int  get()
+	{
+		try
+		{
+			return 1 ;
+		}
+		finally
+		{
+			return 2 ;
+		}
+	}
+}
+
+
+class RunableThread{
+	public static void main(String[] args) {
+		
+		Ticketssell th = new Ticketssell();
+		new Thread(th).start();
+		new Thread(th).start();
+		new Thread(th).start();
+	}
+}
+
+
+class Ticketssell implements Runnable{
+	static int tickets = 120;
+	static Object object = new Object();
+	public void run() {
+		while(true){
+			synchronized (object) {
+				if(tickets > 0){
+					//System.out.printf("\n"+Thread.currentThread().getName()+"现在卖的第%d张票",tickets);
+					System.out.println("\n"+Thread.currentThread().getName()+"现在卖的第"+tickets+"张票");
+				}
+				tickets--;
+				if(tickets <= 0)
+					break;
+			}
+		}
+	}
+}
+
+
+
+
+
